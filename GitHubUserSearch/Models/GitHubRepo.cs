ce4d6ea906007727ee9ed08838e418dba562c9ef
KgothatsoN <kg.otha@hotmail.com>
2025-05-20 14:49:07 +0100

    Fixed repo model spelling

--- GitHubUserSearch/Models/GitHubRepo.cs
+++ GitHubUserSearch/Models/GitHubRepo.cs
@@ -8,7 +8,7 @@ namespace GitHubUserSearch.Models
     public class GitHubRepo
     {
         public string Name { get; set; }
-        public string HtmlUrl { get; set; }
+        public string Html_Url { get; set; }
         public string Description { get; set; }
         public int Stargazers_Count { get; set; }
     }
